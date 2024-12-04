b6976e06a04152b16fedf95c679d4ddab585b9e8
Dragon Ruth <DragonRuth13@gmail.com>
2024-12-04 22:09:25 +0100

    small fix

--- 2024/4/solution.rb
+++ 2024/4/solution.rb
@@ -1,6 +1,6 @@
 require 'matrix'
 follow_dir = lambda {|v,h|
-lambda {|row,col,mtrx| row < 3 && v < 0 ||  row >= mtrx.row_count - 3  && v > 0|| col < 3 && h < 0||  col >=  mtrx.column_count - 3 && h > 0  ? false : (mtrx[row + v*1, col + h*1].nil? ? "" : mtrx[row + v*1, col + h*1]) + (mtrx[row + v*2, col + h*2].nil? ? "" : mtrx[row + v*2, col + h*2])  + (mtrx[row + v*3, col + h*3].nil?  ? "" : mtrx[row + v*3, col + h*3]) == "MAS"}
+lambda {|row,col,mtrx| row < 3 && v < 0 ||  row >= mtrx.row_count - 3  && v > 0|| col < 3 && h < 0||  col >=  mtrx.column_count - 3 && h > 0  ? false : mtrx[row + v*1, col + h*1]  + mtrx[row + v*2, col + h*2]  + mtrx[row + v*3, col + h*3] == "MAS"}
 }
 
 get_count  = lambda {|row,col,mtrx|
